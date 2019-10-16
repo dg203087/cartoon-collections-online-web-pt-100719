50fe3175b7814472e1576d61cc5c337edd9bfc7f
Deanna Kelley <deanna.georges@gmail.com>
2019-10-16 20:10:02 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,5 +1,9 @@
 def roll_call_dwarves(dwarf_names)
-  # Your code here
+  i = 0 
+  while i < dwarf_names.length 
+    puts dwarf_names.index + dwarf name
+    i += 1 
+  end
 end
 
 def summon_captain_planet# code an argument here
